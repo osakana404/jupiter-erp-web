28a0f30ab35d5ea35b5b51b8e3aead23dfb12eca
issx404 <issx404@proton.me>
2026-04-21 15:45:11 +0900

    поправил config.example

--- config.example/db.js
+++ config.example/db.js
@@ -5,6 +5,16 @@ import path from "node:path";
 const sequelize = new Sequelize({
   dialect: "sqlite",
   storage: path.resolve("config", "database.sqlite"),
+  logging: false, // Чтобы не спамить в консоль SQL-запросами (можно включить для дебага)
 });
 
-export { sequelize };
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+}
+
+export { sequelize, testConnection };
